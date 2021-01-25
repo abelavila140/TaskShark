8f572f1cefc3a836015b8c69ae961e4b58bdded1
Abel Avila <abel.avila@sycamoreleaf.com>
2021-01-24 22:35:54 -0600

    Use || as prority to integrate

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -112,7 +112,10 @@ class TasksController < ApplicationController
     end
 
     def task_payload(branch, is_push=false)
-      logger.info "task_payload"
+      branch_task_id = payload['pull_request']['title'].split('|').last.strip
+      task = ClickUp.verify_task_id(branch_task_id)
+      return if task.present?
+
       branch_task_id = branch.split('-').last
       task = ClickUp.verify_task_id(branch_task_id)
       logger.info task.inspect
@@ -121,9 +124,6 @@ class TasksController < ApplicationController
       branch_task_id = branch.split('-cp').last
       task = ClickUp.verify_task_id(branch_task_id)
       return task if task.present? || is_push
-
-      branch_task_id = payload['pull_request']['title'].split('|').last.strip
-      ClickUp.verify_task_id(branch_task_id)
     end
 
     def first_push?
